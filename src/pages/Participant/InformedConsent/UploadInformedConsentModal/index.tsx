55aed6b9bde0f06fc496eafc896cd5b30af46fec
Daniel Jakab <jakdan99@gmail.com>
2025-01-28 16:21:58 +0100

    Add UploadInformedConsentModal

--- src/pages/Participant/InformedConsent/UploadInformedConsentModal/index.tsx
+++ src/pages/Participant/InformedConsent/UploadInformedConsentModal/index.tsx
@@ -1,6 +1,10 @@
 import DragAndDrop from "@Components/DragAndDrop";
 import { Modal } from "@mui/material";
 import { useFormik } from "formik";
+import { useCreateFile } from "@Utils/queries/studies";
+import { useParams } from "react-router";
+import * as yup from "yup";
+import { useTranslation } from "react-i18next";
 import {
   ModalBox,
   ModalTitle,
@@ -17,19 +21,64 @@ type Props = {
   onClose: () => void;
 };
 
-const UploadInformedConsentModal = ({ open, onClose }: Props) => {
-  const fileTypes = ["application/json", "application/pdf", "image/*"];
-  const handleChange = () => {};
-  const uploading = false;
-  const fileName = "";
+const fileTypes = ["application/pdf", "image/"];
 
-  const addProtocolFormik = useFormik({
+const consentSchema = yup.object({
+  file: yup
+    .mixed()
+    .required("Required")
+    .test("fileSize", "File must be smaller than 20MB", (value: File) => {
+      if (!value) return true;
+      const size = value.size / 1024 / 1024;
+      return size < 20;
+    })
+    .test("fileType", "Invalid file type", (value: File) => {
+      if (!value) return true;
+      if (value.type === "application/pdf") return true;
+      return value.type.startsWith("image/");
+    }),
+});
+
+const UploadInformedConsentModal = ({ open, onClose }: Props) => {
+  const { t } = useTranslation();
+  const { participantId, id: studyId } = useParams();
+
+  const uploading = false;
+  const createFile = useCreateFile();
+
+  const addInformedConsentFormik = useFormik({
     initialValues: {
       file: null,
     },
-    validationSchema: null,
-    onSubmit: () => {},
+    validationSchema: consentSchema,
+    onSubmit: async ({ file }: { file: File }) => {
+      const formData = new FormData();
+      formData.append("file", file, file.name);
+      formData.append(
+        "metadata",
+        JSON.stringify({
+          "content-type": file.type,
+          "document-type": "informed_consent",
+          "participant-id": participantId,
+        }),
+      );
+      await createFile.mutateAsync({
+        studyId,
+        formData,
+      });
+      addInformedConsentFormik.resetForm();
+      onClose();
+    },
   });
+
+  const closeModal = () => {
+    addInformedConsentFormik.resetForm();
+    onClose();
+  };
+
+  const handleChange = (file: File) => {
+    addInformedConsentFormik.setFieldValue("file", file);
+  };
 
   return (
     <Modal
@@ -40,10 +89,10 @@ const UploadInformedConsentModal = ({ open, onClose }: Props) => {
     >
       <ModalBox sx={{ boxShadow: 24 }}>
         <ModalTitle variant="h2" id="modal-modal-title">
-          Upload Informed Consent
+          {t("participant:informed_consent.upload.title")}
         </ModalTitle>
         <ModalDescription variant="h5" id="modal-modal-description">
-          Upload a picture or PDF of the informed consent form.
+          {t("participant:informed_consent.upload.description")}
         </ModalDescription>
         <ModalContainer>
           <ModalContent fixHeight>
@@ -51,23 +100,26 @@ const UploadInformedConsentModal = ({ open, onClose }: Props) => {
               handleChange={handleChange}
               fileTypes={fileTypes}
               name="file"
-              formik={addProtocolFormik}
+              formik={addInformedConsentFormik as any}
               uploading={uploading}
-              fileName={fileName}
+              fileName={addInformedConsentFormik.values.file?.name}
             />
           </ModalContent>
         </ModalContainer>
         <ModalActions>
-          <CancelButton variant="text" onClick={onClose}>
-            Cancel
+          <CancelButton variant="text" onClick={closeModal}>
+            {t("common:cancel")}
           </CancelButton>
           <DoneButton
-            disabled={!addProtocolFormik.dirty || !addProtocolFormik.isValid}
+            disabled={
+              !addInformedConsentFormik.dirty ||
+              !addInformedConsentFormik.isValid
+            }
             variant="contained"
             sx={{ elevation: 0 }}
-            onClick={() => addProtocolFormik.handleSubmit()}
+            onClick={() => addInformedConsentFormik.handleSubmit()}
           >
-            Add
+            {t("common:add")}
           </DoneButton>
         </ModalActions>
       </ModalBox>
