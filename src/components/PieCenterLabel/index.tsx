d0ae9cd13c184ed46ca7020a4a8df0970ae51a79
Daniel Jakab <jakdan99@gmail.com>
2025-05-07 12:32:55 +0200

    Update packages, fix issues

--- src/components/PieCenterLabel/index.tsx
+++ src/components/PieCenterLabel/index.tsx
@@ -5,10 +5,10 @@ const PieCenterLabel = ({ children }: { children: React.ReactNode }) => {
   const { width, height, left, top } = useDrawingArea();
   return (
     <text x={left + width / 2} y={top + height / 2}>
-      <StyledNumberTSpan x={left + width} dy="-0.3em">
+      <StyledNumberTSpan x={left + width / 2} dy="-0.3em">
         {children}
       </StyledNumberTSpan>
-      <StyledTSpan x={left + width} dy="2em">
+      <StyledTSpan x={left + width / 2} dy="2em">
         Deployments
       </StyledTSpan>
     </text>
